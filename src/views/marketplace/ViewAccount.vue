4ba0ebd4f3e555078510773a75e3d5d3a5a63a4c
Denis Wanjohi <denniswanjohi.m@gmail.com>
2024-09-03 21:02:49 +0300

    Hello

--- src/views/marketplace/ViewAccount.vue
+++ src/views/marketplace/ViewAccount.vue
@@ -36,8 +36,68 @@
                         {{ `COST: ${formatMoney(getCurrentAccountInView.cost)} ( ` }}
                         {{ getCurrentAccountInView.negotiable ? 'Negotiable )' : 'Non-negotiable )' }}
                     </v-col>
+                    <v-col class="col-12">
+                        {{ "SCREENSHOTS" }}
+                    </v-col>
+                    <v-col class="col-12 mb-2" v-for="(ima, i) in 6" :key="i">
+                        <v-img class="" :src="'https://kazibin.nyc3.cdn.digitaloceanspaces.com/Accounts/IJ-4XT/mTjyNxeknREyoul5q3TwZha6THfjvBOj5zWJxb1W.png'" contain />
+                    </v-col>
+
+                    <div class="d-flex justify-end call-button">
+                        <v-menu 
+                        transition="slide-y-transition"
+                        bottom
+                        open-on-hover>
+                        <template v-slot:activator="{ on, attrs }">
+                        <span class="white--text pointer"
+                          v-bind="attrs"
+                          v-on="on">
+                          <v-btn small 
+                          class="elevation- ml-2 red lighten-2 white--text" 
+                          style="font-weight: 900;" 
+                          :loading="false">
+                              Contact Seller
+                          </v-btn>
+                        </span>
+                        </template>
+                        <v-list>
+                            <v-list-item
+                            class="pointer"
+                            @click="call">
+                                <v-icon small class="mr-2">
+                                    mdi-phone
+                                </v-icon>
+                                Call
+                            </v-list-item>
+                            <v-list-item
+                            class="pointer"
+                            @click="sms">
+                                <v-icon small class="mr-2">
+                                    mdi-message
+                                </v-icon>
+                                SMS
+                            </v-list-item>
+                            <v-list-item
+                            class="pointer"
+                            @click="sendEmail">
+                                <v-icon small class="mr-2">
+                                    mdi-mail
+                                </v-icon>
+                                Email
+                            </v-list-item>
+                            <v-list-item
+                            class="pointer"
+                            @click="goToWhatsApp">
+                                <v-icon small class="mr-2">
+                                    mdi-whatsapp
+                                </v-icon>
+                                WhatsApp
+                            </v-list-item>
+                        </v-list>
+                        </v-menu>
+                    </div>
                 </v-row>
-                Images go here
+
             </section>
         </div>
         <div v-else>
@@ -77,6 +137,50 @@ export default{
                 currency: 'KES',
             });
             return money_format.format(cost)
+        },
+
+        goToWhatsApp() {
+            // Encode the message to be URL-safe
+            let message = `Hello ${this.account.user.username},\n\nI hope this message finds you well. I am interested in the ${this.account.title} writing account advertised on kazibin (${process.env.VUE_APP_FRONT_END_URL}/m/${this.account.code}).  Could you please provide more details about the account?\n\nLooking forward to your response.\n\nBest regards,\n${this.getUser.username}`;
+
+            let encodedMessage = encodeURIComponent(message)
+
+            // Construct the WhatsApp URL with the phone number and message
+            let whatsappUrl = `https://wa.me/+254${this.account.user.phone_number}?text=${encodedMessage}`;
+
+            // Open WhatsApp with the pre-filled message
+            window.open(whatsappUrl);
+        },
+
+        call(){
+            const phone_number = '+254' + this.account.user.phone_number
+            window.location.href = 'tel:' + phone_number;
+        },
+
+        sms(){
+            // Encode the message to be URL-safe
+            let message = `Hello ${this.account.user.username},\n\nI hope this message finds you well. I am interested in the ${this.account.title} writing account advertised on kazibin (${process.env.VUE_APP_FRONT_END_URL}/m/${this.account.code}).  Could you please provide more details about the account?\n\nLooking forward to your response.\n\nBest regards,\n${this.getUser.username}`;
+
+            let encodedMessage = encodeURIComponent(message)
+
+            // Construct the sms: URI with the phone number and message body
+            let smsUri = `sms:+254${this.account.user.phone_number}?body=${encodedMessage}`
+
+            window.location.href = smsUri;
+        },
+
+        sendEmail() {
+            let encodedSubject = `INTEREST IN THE ${this.account.title.toUpperCase()} ACCOUNT`
+            
+            // Encode the message to be URL-safe
+            let message = `Hello ${this.account.user.username},\n\nI hope this message finds you well. I am interested in the ${this.account.title} writing account advertised on kazibin (${process.env.VUE_APP_FRONT_END_URL}/m/${this.account.code}).  Could you please provide more details about the account?\n\nLooking forward to your response.\n\nBest regards,\n${this.getUser.username}`;
+
+            let encodedBody = encodeURIComponent(message)
+
+            let mailtoUri = `mailto:${this.account.user.email}?subject=${encodedSubject}&body=${encodedBody}`;
+
+            // Open the email client with the pre-filled information
+            window.location.href = mailtoUri;
         },
 
         boot() {
@@ -120,6 +224,13 @@ export default{
     height: calc(100vh - 50px);
     overflow-y: scroll;
     overflow-x: hidden;
+    position: relative;
+}
+
+.call-button{
+    position: fixed;
+    bottom: 0.5rem;
+    right: 0.5rem;
 }
 </style>
 
